e6df9d11a1426f22ebe0854b163cf60bc0543308
solomon <shaminsolomonit@gmail.com>
2019-04-21 08:50:28 +0530

    "just adding modules"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { ListproductComponent } from './listproduct/listproduct.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddproductComponent,
+    ListproductComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
